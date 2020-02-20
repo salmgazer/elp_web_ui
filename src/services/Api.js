b2a681d268b76336014c8d9852dee12db4e4802e
Blatacharry <fredobiriyeboah@gmail.com>
2020-02-20 20:31:13 +0000

    Search mode update

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,3 +3,14 @@ import axios from 'axios';
 export default axios.create({
     baseURL: `https://elparah.store`
 });
+
+
+
+class Api {
+    static get basePath() {
+        return "";
+    }
+
+
+    index(basePath = this.constructor.basePath, queryParams={})
+}
