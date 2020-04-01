a3a3639162b7901b7b6db1de81eee77cd9791075
Blatacharry <fredobiriyeboah@gmail.com>
2020-04-01 11:36:08 +0000

    Made changes to ModelAction

--- src/services/ModelAction.js
+++ src/services/ModelAction.js
@@ -8,7 +8,6 @@ export default class ModelAction {
         this.table = models[model].table;
         this.database = database;
         this.columns = models[model].columns;
-        //console.log(this.columns.name)
     }
 
     /*
@@ -16,8 +15,6 @@ export default class ModelAction {
     * @return array of items
     * */
     async index(){
-        const data = await database.collections.get(this.table).query().observe();
-        console.log(data);
         return await database.collections.get(this.table).query().observe();
     }
 
@@ -28,8 +25,6 @@ export default class ModelAction {
     async findById(id){
         const dataCollection = database.collections.get(this.table);
 
-        console.log(id);
-        console.log(await dataCollection.find(id))
         return await dataCollection.findAndObserve(id);
     }
 
@@ -105,50 +100,38 @@ export default class ModelAction {
         })
     }
 
+    /*
+    * @todo query like the available query parameters
+    * */
     async queryType(column) {
         switch (column.fxn) {
             case '':
                 return Q.where(column.name , column.value);
-                break;
             case 'notEq':
                 return Q.where(column.name , Q.notEq(column.value));
-                break;
             case 'gt':
                 return Q.where(column.name , Q.gt(column.value));
-                break;
             case 'weakGt':
                 return Q.where(column.name , Q.weakGt(column.value));
-                break;
             case 'gte':
                 return Q.where(column.name , Q.gte(column.value));
-                break;
             case 'lt':
                 return Q.where(column.name , Q.lt(column.value));
-                break;
             case 'lte':
                 return Q.where(column.name , Q.lte(column.value));
-                break;
             case 'between':
                 return Q.where(column.name , Q.between(column.min , column.max));
-                break;
             case 'oneOf':
                 return Q.where(column.name , Q.oneOf(column.value));
-                break;
             case 'notIn':
                 return Q.where(column.name , Q.notIn(column.value));
-                break;
             case 'like':
                 return Q.where(column.name , Q.like(`%${Q.sanitizeLikeString(column.value)}%`));
-                break;
             case 'notLike':
                 return Q.where(column.name , Q.notLike(`%${Q.sanitizeLikeString(column.value)}%`));
-                break;
             default:
                 return Q.where(column.name , Q.eq(column.value));
 
         }
     }
-    /*
-    * @todo query like the available query parameters
-    * */
 }
