22067a67189463bc94a020c3b6dace9d76b6dc39
Blatacharry <fredobiriyeboah@gmail.com>
2020-06-19 18:14:24 +0000

    Login loader fix

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,42 +1,15 @@
 import React from 'react';
-import BounceLoader from "react-spinners/BounceLoader";
-import {
-    makeStyles,
-} from '@material-ui/core/styles';
-import Typography from "@material-ui/core/Typography/Typography";
-
-const useStyles = makeStyles({
-   root: {
-       color: '#DAAB59',
-       display: 'flex',
-       width: '100%',
-       height: '100%',
-       textAlign: 'center',
-       justifyContent: 'center',
-       alignItems: 'center',
-   }
-});
+import Loader from 'react-loader-spinner';
 
 const PrimaryLoader = (props) => {
-    const styles = useStyles();
-
     return(
-        <div className={styles.root}>
-            <BounceLoader
-                size={props.size || 60}
-                color={props.color || "#DAAB59"}
-                loading={props.state || true}
-            /><br/>
-
-            <Typography
-                component="h5"
-                variant="h5"
-                style={{fontWeight: '400', fontSize: '16px' , padding: '14px'}}
-                className={`text-center mx-auto`}
-            >
-                {props.text}
-            </Typography>
-        </div>
+        <Loader
+            type={props.type}
+            color={props.color}
+            height={props.height}
+            width={props.width}
+            timeout={props.timeout}
+        />
     );
 };
 
---
+++ src/components/Loader/componentLoader.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import BounceLoader from "react-spinners/BounceLoader";
+import {
+    makeStyles,
+} from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography/Typography";
+
+const useStyles = makeStyles({
+    root: {
+        color: '#DAAB59',
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+const ComponentLoader = (props) => {
+    const styles = useStyles();
+
+    return(
+        <div className={styles.root}>
+            <BounceLoader
+                size={props.size || 60}
+                color={props.color || "#DAAB59"}
+                loading={props.state || true}
+            /><br/>
+
+            <Typography
+                component="h5"
+                variant="h5"
+                style={{fontWeight: '400', fontSize: '16px' , padding: '14px'}}
+                className={`text-center mx-auto`}
+            >
+                {props.text}
+            </Typography>
+        </div>
+    );
+};
+
+export default ComponentLoader;
