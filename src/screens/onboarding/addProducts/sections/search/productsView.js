30c27bb934431c9deb6f9a14d4f99a74ffc074b1
Blatacharry <fredobiriyeboah@gmail.com>
2020-06-29 02:32:29 +0000

    Add product fix

--- src/screens/onboarding/addProducts/sections/search/productsView.js
+++ src/screens/onboarding/addProducts/sections/search/productsView.js
@@ -5,10 +5,6 @@ import AddIcon from "../../../../../components/ClickableIcons/AddIcon";
 import ProductCard from "../../../../../components/Cards/ProductCard";
 
 const ProductsView = (props) => {
-    console.log('&&&&&&&&&&&&&&&&&&&')
-    console.log(props.products)
-    console.log('&&&&&&&&&&&&&&&&&&&')
-
     const addProductHandler = (id) => {
         props.addProductHandler(id);
     };
