2b3e71f56adf688ed81e266e201c2df5ac54cfca
Blatacharry <fredobiriyeboah@gmail.com>
2020-07-16 12:55:03 +0000

    Search on subcategories added

--- src/screens/onboarding/categorySetup/CategorySetup.js
+++ src/screens/onboarding/categorySetup/CategorySetup.js
@@ -10,6 +10,7 @@ import Step2 from "./sections/Step2";
 import Api from "../../../services/Api";
 import SimpleSnackbar from "../../../components/Snackbar/SimpleSnackbar";
 import { Grid } from '@material-ui/core';
+import LocalInfo from "../../../services/LocalInfo";
 
 class CategorySetup extends Component{
     constructor(props) {
@@ -23,7 +24,9 @@ class CategorySetup extends Component{
             errorMsg: '',
             activeItem: 0,
             loading: true,
+            addedCategories: [],
             counter: 0,
+            search: false,
         };
 
         this.currentPathname = null;
@@ -52,24 +55,42 @@ class CategorySetup extends Component{
             }
         });
 
-        const items = JSON.parse(localStorage.getItem('categoryLookup')) || [];
+        /*const items = JSON.parse(localStorage.getItem('categoryLookup')) || [];
         this.setState({
             shop_subcategories: items.filter((item) => item.owned === true)
-        }) 
+        })*/
 
-        const branchId = localStorage.getItem('activeBranch');
-        const accessToken = localStorage.getItem('accessToken');
+        const branchId = LocalInfo.branchId;
+        const accessToken = LocalInfo.accessToken;
         try {
             let newCategory = await new Api('others').index(
                 {},
                 {'Authorization': `Bearer ${accessToken}`},
                 `https://${Api.apiDomain()}/v1/client/branches/${branchId}/product_categories`,
             );
-            localStorage.setItem('categoryLookup' , JSON.stringify(newCategory.data.allChildren));
+
+            let localCategories = newCategory.data.allChildren;
+            /*const storageCategories = JSON.parse(localStorage.getItem("branchCategoriesAdded")) || [];
+            const removedCategories = JSON.parse(localStorage.getItem("branchCategoriesARemoved")) || [];
+
+            if(storageCategories.length > 0){
+                for (let i = 0; i < storageCategories.length; i++) {
+                    console.log('me')
+                    localCategories = await this.startupAddNewCategories(storageCategories[i] , localCategories);
+                }
+            }*/
+
+            /*if(removedCategories.length > 0){
+                for (let i = 0; i < removedCategories.length; i++) {
+                    localCategories = await this.startupRemoveNewCategories(removedCategories[i] , localCategories);
+                }
+            }*/
+
+            localStorage.setItem('categoryLookup' , JSON.stringify(localCategories));
 
             await this.setState({
                 'categories' : newCategory.data.allParents,
-                'subcategories' : newCategory.data.allChildren,
+                'subcategories' : localCategories,
                 'counter': ((JSON.parse(localStorage.getItem('categoryLookup'))).filter(item => item.owned === true)).length
             });
 
@@ -77,6 +98,34 @@ class CategorySetup extends Component{
             console.log(error)
         }
     }
+
+    /*startupAddNewCategories = async(id , products) => {
+        console.log(products)
+        console.log(id)
+        let old_list = products;
+
+        const productIndex = old_list.findIndex((item => item.id === (id)));
+        const item = await {...old_list[productIndex]};
+        console.log(item)
+        if(!item.owned){
+            item.owned = true;
+        }
+        console.log(old_list)
+
+        return old_list;
+    };
+
+    startupRemoveNewCategories = async(id , products) => {
+        let old_list = products;
+
+        const productIndex = old_list.findIndex((item => item.id === (id)));
+        const item = {...old_list[productIndex]};
+        if(item.owned){
+            item.owned = false;
+        }
+
+        return old_list;
+    };*/
 
     selectCategoryHandler = (itemId , event) => {
         //const stateSubcategories = this.state.subcategories;
@@ -173,15 +222,24 @@ class CategorySetup extends Component{
         * @todo
         * Work on fetchin data from source
         * */
-        const old_subcategories = JSON.parse(localStorage.getItem('categoryLookup'));
+        const items = JSON.parse(localStorage.getItem('categoryLookup')) || [];
+        const shop_subcategories = items.filter((item) => item.owned === true);
 
-        const subcategories = old_subcategories.filter(function(item) {
+        const subcategories = shop_subcategories.filter(function(item) {
             return (item.name).toLowerCase().indexOf(search.toLowerCase()) !== -1
         });
 
-        this.setState({
-            shop_subcategories: subcategories
-        });
+        if(search.length > 0){
+            this.setState({
+                shop_subcategories: subcategories,
+                search: true
+            });
+        }else{
+            this.setState({
+                shop_subcategories: subcategories,
+                search: false
+            });
+        }
     };
 
     //Steps to select category
@@ -192,8 +250,8 @@ class CategorySetup extends Component{
     getStepContent = step => {
         const categories = this.state.categories;
         const subcategories = this.state.subcategories;
-        //const items = JSON.parse(localStorage.getItem('categoryLookup')) || [];
-        const shop_subcategories = this.state.shop_subcategories;
+        const items = JSON.parse(localStorage.getItem('categoryLookup')) || [];
+        const shop_subcategories = this.state.search ? this.state.shop_subcategories : items.filter((item) => item.owned === true);
 
         switch (step) {
             case 0:
